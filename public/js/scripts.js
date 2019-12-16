d5feee4c6d1851df99f58257410a802fcbb6774b
Uko Akanowo <ukoakanowo98@gmail.com>
2019-12-16 22:11:41 +0100

    Fully fixed everything

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,22 +1,91 @@
 //jshint esversion:6
+$(document).ready(function () {
+  $('select').formSelect();
+  var instance = M.FormSelect.getInstance();
+  instance.getSelectedValues();
+});
+
 let errorMessage = document.getElementById('errorMessage');
 
+// Validate Login form
 function validateForm() {
   let username = "Maria";
   let passwordValue = 123;
   let name = document.forms.login.name.value;
   let password = document.forms.login.password.value;
-  if(name == username && password == passwordValue) {
+  if (name == username && password == passwordValue) {
     return true;
-  }
-  else {
+  } else {
     errorMessage.innerHTML = 'Invalid login details';
-      return false;
+    return false;
   }
 }
 
-$(document).ready(function () {
-  $('select').formSelect();
-  var instance = M.FormSelect.getInstance();
-  instance.getSelectedValues();
-}); 
+// Array to store the bank names gotten from the AJAX call
+let bankNames = [];
+
+//Set the selected bank value to the input box value
+function setBankName() {
+  var val = document.getElementById('bankName').value;
+  if (val.trim() != "") {
+    var opts = document.getElementById('banks').childNodes;
+    for (var i = 0; i < opts.length; i++) {
+      if (opts[i].value !== undefined) {
+        if (opts[i].value === val) {
+          document.getElementById('bankName').value = opts[i].innerText;
+          break;
+        }
+      }
+    }
+  } else {
+    document.getElementById('bank_name').innerText = "";
+  }
+}
+
+// Function to fecth the banks
+let fetchBank = () => {
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      let response = JSON.parse(xhr.response);
+      let banks = response.data;
+      banks.forEach(bank => {
+        let bankDetails = bank;
+        bankNames.push(bankDetails);
+      });
+
+      //Create and append select list
+      let dataList = document.getElementById("banks");
+
+      //Create and append the options
+      bankNames.forEach(bankName => {
+        let option = document.createElement("option");
+        console.log(bankName);
+        option.value = bankName.name;
+        option.text = bankName.name;
+        dataList.appendChild(option);
+
+        // Autogenerate the code
+        let item = document.getElementById('bankName');
+        item.onchange = function () {
+          let values = [];
+          let selectedValue = document.getElementById('bankName').value;
+          values.push(selectedValue);
+          console.log(values);
+          bankNames.filter(val => {
+            if (values.includes(val.name)) {
+              document.getElementById('bankCode').value = val.code;
+            }
+          });
+
+        };
+      });
+    }
+  };
+  xhr.open("GET", `https://api.paystack.co/bank`);
+  xhr.setRequestHeader("Authorization", "Bearer sk_test_d09893caef32725fbd69191965eb39d1d9597cd4");
+  xhr.send();
+
+};
+
+fetchBank();
--- server.js
+++ server.js
@@ -17,24 +17,24 @@ app.set("view engine", "ejs");
 
 // Fetch the list of banks
 
-let fetchBankDetails = () => {
-    let options = {
-        url: 'https://api.paystack.co/bank',
-        Authorization: 'Bearer sk_test_d09893caef32725fbd69191965eb39d1d9597cd4'
-    };
-    request(options, (error, response, body) => {
-        let data = JSON.parse(body);
-        let banks = data.data;
-        console.log(response.statusCode);
-        banks.forEach(bank => {
-            let bankDetails = bank;
-            bankList.push(bankDetails);
-        });
+// let fetchBankDetails = () => {
+//     let options = {
+//         url: 'https://api.paystack.co/bank',
+//         Authorization: 'Bearer sk_test_d09893caef32725fbd69191965eb39d1d9597cd4'
+//     };
+//     request(options, (error, response, body) => {
+//         let data = JSON.parse(body);
+//         let banks = data.data;
+//         console.log(response.statusCode);
+//         banks.forEach(bank => {
+//             let bankDetails = bank;
+//             bankList.push(bankDetails);
+//         });
         
-    });
-};
+//     });
+// };
 
-fetchBankDetails();
+// fetchBankDetails();
 
 // Get request to the home route
 
@@ -52,16 +52,14 @@ app.post('/', (_req, _res) => {
 
 // Get request to the banks route
 app.get('/banks', (_req, _res) => {
-    _res.render('banks', {
-        banks: bankList,
-    });
+    _res.render('banks');
 });
 
 // Post request to the banks route
 
 app.post('/banks', (_req, _res) => {
-    let bank_code = _req.body.bank_code;
-    let account_number = _req.body.account_number;
+    let bank_code = _req.body.bankCode;
+    let account_number = _req.body.accountNumber;
     let options = {
         url: `https://api.paystack.co/bank/resolve?account_number=${account_number}&bank_code=${bank_code}`,
         headers: {
@@ -70,8 +68,6 @@ app.post('/banks', (_req, _res) => {
     };
     request(options, (error, response, body) => {
         let jsonData = JSON.parse(body);
-        console.log(response.statusCode);
-        console.log(jsonData);
         if(response.statusCode === 200) {
             _res.render('success', {
                 success: 'Account number found!! The account name is ' + jsonData.data.account_name
@@ -86,8 +82,8 @@ app.post('/banks', (_req, _res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000 \nCopy link in browser: http://localhost:3000 ');
+app.listen(8080, () => {
+    console.log('Server started on port 3000 \nCopy link in browser: http://localhost:8080 ');
 
 });
 
